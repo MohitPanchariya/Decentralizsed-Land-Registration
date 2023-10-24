14904408da18d0132095f5eb840e3c2222c15b0a
Ahmed Hamzah <probablyhamzah@gmail.com>
2023-10-24 21:08:38 +0530

    Update registerAccount-contract.sol

--- registerAccount-contract.sol
+++ registerAccount-contract.sol
@@ -1,14 +1,19 @@
 pragma solidity ^0.8.0;
 
+//contract Accounts
 contract LandRegistrationSystem {
     address public deployer;
     
+
     struct UserAccount {
         string username;
-        bool isLandInspector;
+        bool isVerified;
+        uint256 authority; // 0: no authority, 1: land inspector: 2: second level authority
+        
     }
 
     mapping(address => UserAccount) public userAccounts;
+    mapping(uint256 => Land) public lands;
     
     constructor() {
         deployer = msg.sender;
@@ -17,12 +22,10 @@ contract LandRegistrationSystem {
 
     modifier onlyDeployer() {
         require(msg.sender == deployer, "Only the deployer can perform this action");
-        _;
     }
 
     modifier onlyLandInspector() {
-        require(userAccounts[msg.sender].isLandInspector, "Only land inspectors can perform this action");
-        _;
+        require(userAccounts[msg.sender].authority == 1, "Only land inspectors can perform this action");
     }
 
     function addLandInspector(address _inspector, string memory _username) public onlyDeployer {
@@ -35,6 +38,27 @@ contract LandRegistrationSystem {
     }
 
     function isLandInspector(address _account) public view returns (bool) {
-        return userAccounts[_account].isLandInspector;
+        return userAccounts[_account].authority == 1;
+    }
+
+    function verifyAccount(uint256 _aadharNumber, string panNumber) public {
+        //Make sure aadhar is valid, pan is valid. Try to search online how this can be done.
+        //Make sure no person can register twice, i.e no two different addresses should have same
+        //aadhar or pan number.
+    }
+    
+    /*
+        Returns a boolean for whether the account is verified.
+    */
+    function isVerified(address _account) public view returns(bool){
+        return userAccounts[_account].isVerified;
+    }
+    
+    /*
+        Allowing first and second level authorities to grant inspector status to an account.
+    */
+    function grantLandInspectorStatus(address _account) public {
+        if (userAccounts[msg.sender].authority == 1 || userAccounts[msg.sender].authority == 2)
+            userAccounts[_account].authority = 1;
     }
 }
