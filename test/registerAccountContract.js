e62045f89374a43450f77c286fe735472a5a3608
Ahmed Hamzah <probablyhamzah@gmail.com>
2023-10-30 22:35:52 +0530

    updated tests
    Updated tests for adding and removing a second-level authority.

--- test/registerAccountContract.js
+++ test/registerAccountContract.js
@@ -26,16 +26,6 @@ contract("AccountRegistration", (accounts) => {
         }
     });
 
-    it("should allow adding a Land Inspector by deployer or second-level authority", async () => {
-        await accountRegistration.addLandInspector(accounts[1], "Inspector1", 222222222223, {
-            from: accounts[0],
-        });
-
-        const inspector = await accountRegistration.userAccounts(accounts[1]);
-        assert.equal(inspector.username, "Inspector1", "Inspector username is not set correctly");
-        assert.equal(inspector.designation, 1, "Inspector designation is not set correctly");
-        assert.equal(inspector.aadharNumber, 222222222223, "Inspector Aadhar number is not set correctly");
-    });
 
     it("should not allow adding a Land Inspector with an existing Aadhar number", async () => {
         try {
@@ -65,16 +55,6 @@ contract("AccountRegistration", (accounts) => {
         }
     });
 
-    it("should allow adding a Second-Level Authority by deployer", async () => {
-        await accountRegistration.addSecondLevelAuthority(accounts[4], "Authority1", 444444444444, {
-            from: accounts[0],
-        });
-
-        const authority = await accountRegistration.userAccounts(accounts[4]);
-        assert.equal(authority.username, "Authority1", "Authority username is not set correctly");
-        assert.equal(authority.designation, 2, "Authority designation is not set correctly");
-        assert.equal(authority.aadharNumber, 444444444444, "Authority Aadhar number is not set correctly");
-    });
 
     it("should not allow adding a Second-Level Authority with an existing Aadhar number", async () => {
         try {
@@ -104,14 +84,6 @@ contract("AccountRegistration", (accounts) => {
         }
     });
 
-    it("should allow removing a Second-Level Authority by deployer", async () => {
-        await accountRegistration.removeSecondLevelAuthority(accounts[4], {
-            from: accounts[0],
-        });
-
-        const authority = await accountRegistration.userAccounts(accounts[4]);
-        assert.equal(authority.designation, 0, "Authority designation is not reset correctly");
-    });
 
     it("should not allow removing the deployer's Second-Level Authority privileges", async () => {
         try {
@@ -128,17 +100,13 @@ contract("AccountRegistration", (accounts) => {
     });
 
     it("should allow removing a Land Inspector by deployer or second-level authority", async () => {
-        await accountRegistration.addLandInspector(accounts[1], "Inspector1", 666666666666, {
-            from: accounts[0],
-        });
-
-        await accountRegistration.removeLandInspector(accounts[1], {
-            from: accounts[0],
-        });
-
-        const inspector = await accountRegistration.userAccounts(accounts[1]);
-        assert.equal(inspector.designation, 0, "Inspector designation is not reset correctly");
+    await accountRegistration.removeLandInspector(accounts[2], {
+      from: accounts[0],
     });
+
+    const inspector = await accountRegistration.userAccountsMap(accounts[2]);
+    assert.equal(inspector.designation, 0, "Inspector designation is not reset correctly");
+  });
 
     it("should not allow removing the deployer's privileges", async () => {
         try {
@@ -163,6 +131,31 @@ contract("AccountRegistration", (accounts) => {
         const isDeployer = await accountRegistration.isDeployer(accounts[0]);
         assert.equal(isDeployer, true, "Account should be a deployer");
     });
+    
+    it("should not allow removing the deployer's Second-Level Authority privileges", async () => {
+        try {
+          await accountRegistration.removeSecondLevelAuthority(accounts[0], {
+            from: accounts[0],
+          });
+          assert.fail("Should have thrown an error");
+        } catch (error) {
+          assert(
+            error.message.includes("Cannot remove deployer's Second-level Authority privileges"),
+            "Expected 'Cannot remove deployer's Second-level Authority privileges' error message"
+          );
+        }
+      });
+      
+      it("should allow the deployer to add a Second-Level Authority", async () => {
+        await accountRegistration.addSecondLevelAuthority(accounts[4], "Authority1", 333333333333, {
+          from: accounts[0],
+        });
+
+        const authority = await accountRegistration.userAccountsMap(accounts[4]);
+        assert.equal(authority.username, "Authority1", "Authority username is not set correctly");
+        assert.equal(authority.designation, 2, "Authority designation is not set correctly");
+        assert.equal(authority.aadharNumber, 333333333333, "Authority Aadhar number is not set correctly");
+      });
 
     it("should not allow non-deployer or non-second-level authority to grant Land Inspector status", async () => {
         try {
@@ -176,22 +169,6 @@ contract("AccountRegistration", (accounts) => {
                 "Error not thrown"
             );
         }
-    });
-
-    it("should grant Second-Level Authority status by deployer", async () => {
-        await accountRegistration.grantSecondLevelAuthorityStatus(accounts[1], {
-            from: accounts[0],
-        });
-
-        const authority = await accountRegistration.userAccounts(accounts[1]);
-        assert.equal(authority.designation, 2, "Authority designation is not set correctly");
-
-        await accountRegistration.grantSecondLevelAuthorityStatus(accounts[2], {
-            from: accounts[0],
-        });
-
-        const authority2 = await accountRegistration.userAccounts(accounts[2]);
-        assert.equal(authority2.designation, 2, "Authority designation is not set correctly");
     });
 
     it("should not allow non-deployer to grant Second-Level Authority status", async () => {
@@ -208,4 +185,3 @@ contract("AccountRegistration", (accounts) => {
         }
     });
 });
-
