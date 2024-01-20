37e298d411475c547c01b74ea2643f3f218d7ed0
Manasvi <mamore@students.vnit.ac.in>
2024-01-21 00:25:42 +0530

    add user-details route

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -5,12 +5,13 @@ import Home from "./Components/Home/Home.jsx"
 import {LoginSignup} from "./Components/LoginSignup/LoginSignup.jsx"
 import {AddLandRecord} from "./Components/AddLands/AddLand.jsx"
 import LandsForSale from './Components/LandsForSale/LandsForSale.jsx';
+import UserDetails from './Components/UserDetails/UserDetails.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<LoginSignup/>} />
-      {/* <Route path='/login' element={<LoginSignup/>} /> */}
+      <Route path='/user-details' element={<UserDetails/>} /> 
       <Route path="/home" element={<Home/>} />
       <Route path='/add-land' element={<AddLandRecord/>} />
       <Route path="/for-sale" element={<LandsForSale/>} />
