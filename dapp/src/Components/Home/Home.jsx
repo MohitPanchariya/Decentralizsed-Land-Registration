b64082185a5a0207cab0caf5ab912847db8e3104
Manasvi <mamore@students.vnit.ac.in>
2024-01-22 00:15:54 +0530

    Add CSS

--- dapp/src/Components/Home/Home.jsx
+++ dapp/src/Components/Home/Home.jsx
@@ -94,24 +94,24 @@ function Home() {
       <div className="card-container">
         {myLands.map((land, index) => (
           <div className="card" key={index}>
-            <h3>Land {index + 1}</h3>
-            <p>Land Id: {land.landId.toString()}</p>
+            <center><h3>LAND {index + 1}</h3></center>
+            <p className="land-details"><b>Land Id: </b>{land.landId.toString()}</p>
             {/* Land Owner Address */}
-            <p>Land Owner Address: {land.owner}</p>
-            <p>State: {land.identifier.state}</p>
-            <p>Division: {land.identifier.division}</p>
-            <p>District: {land.identifier.district}</p>
-            <p>Taluka: {land.identifier.taluka}</p>
-            <p>Village: {land.identifier.village}</p>
-            <p>Survery Number: {land.identifier.surveyNumber.toString()}</p>
-            <p>Subdivision: {land.identifier.subdivision}</p>
-            <p>Area: {land.area.toString()}</p>
-            <p>Purchase Date: {land.purchaseDate.toString()}</p>
-            <p>Purchase Price: {land.purchasePrice.toString()}</p>
-            <p>Land Value at Purchase: {land.landValueAtPurchase.toString()}</p>
-            <p>Land Verified: {land.isVerified.toString()}</p>
-            <p>Land for sale: {land.isForSale.toString()}</p>
-            <button
+            <p className="land-details"><b>Land Owner Address: </b>{land.owner}</p>
+            <p className="land-details"><b>State: </b>{land.identifier.state}</p>
+            <p className="land-details"><b>Division: </b>{land.identifier.division}</p>
+            <p className="land-details"><b>District: </b>{land.identifier.district}</p>
+            <p className="land-details"><b>Taluka: </b>{land.identifier.taluka}</p>
+            <p className="land-details"><b>Village: </b>{land.identifier.village}</p>
+            <p className="land-details"><b>Survery Number: </b>{land.identifier.surveyNumber.toString()}</p>
+            <p className="land-details"><b>Subdivision: </b>{land.identifier.subdivision}</p>
+            <p className="land-details"><b>Area: </b>{land.area.toString()}</p>
+            <p className="land-details"><b>Purchase Date: </b>{land.purchaseDate.toString()}</p>
+            <p className="land-details"><b>Purchase Price: </b>{land.purchasePrice.toString()}</p>
+            <p className="land-details"><b>Land Value at Purchase: </b>{land.landValueAtPurchase.toString()}</p>
+            <p className="land-details"><b>Land Verified: </b>{land.isVerified.toString()}</p>
+            <p className="land-details"><b>Land for sale: </b>{land.isForSale.toString()}</p>
+            <button className="submit-list"
               onClick={() => {
                 listLandForSale(land.landId);
               }}
--- dapp/src/Components/LandsForSale/LandsForSale.jsx
+++ dapp/src/Components/LandsForSale/LandsForSale.jsx
@@ -1,94 +1,142 @@
 import React, { useEffect, useState } from "react";
-import configuration from "../../LandRegistration.json"
+import configuration from "../../LandRegistration.json";
 import Web3 from "web3";
-import "./LandsForSale.css"
-import Sidebar from "../Sidebar/Sidebar"
+import Sidebar from "../Sidebar/Sidebar";
 
-const landContractAddress = "0x030722FDC11E466544368d96fDEa6CD31411c282";
+const landContractAddress = "0x76017b4E9Fe30D5b2Ba7D345B8a42aC2b85C7978";
 const contractABI = configuration.abi;
 
 //Returns jsx which will display all lands owned by the user
 function LandsForSale() {
-    const [landsForSale, setLandsForSale] = useState([]);
+  const [landsForSale, setLandsForSale] = useState([]);
 
-    const getMetamaskAccount = async() => {
-        // Request MetaMask account access
-        const accounts = await window.ethereum.request({
-            method: "eth_requestAccounts",
-        });
-        console.log(accounts[0]);
-        return accounts[0];
+  const getMetamaskAccount = async () => {
+    // Request MetaMask account access
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    console.log(accounts[0]);
+    return accounts[0];
+  };
+
+  const fetchLandsForSale = async (account) => {
+    // Create a Web3 instance using the provider from MetaMask
+    const web3Instance = new Web3(window.ethereum);
+
+    // Connect to your contract using the Web3 instance
+    const contract = new web3Instance.eth.Contract(
+      contractABI,
+      landContractAddress
+    );
+
+    const gas = 2000000;
+    // Call the getLandsForSale function on the contract
+    const result = await contract.methods
+      .getLandsForSale()
+      .call({ from: account, gas });
+    console.log(result);
+    return result;
+  };
+
+  const initPage = async () => {
+    if (window.ethereum) {
+      try {
+        const account = await getMetamaskAccount();
+        console.log(account);
+        const result = await fetchLandsForSale(account);
+        setLandsForSale(result);
+      } catch (error) {
+        console.error(error);
+        console.error("Error connecting to Metamask:", error);
+      }
+    } else {
+      if (!window.ethereum) {
+        console.error("Metamask not detected");
+      } else {
+        console.log("Already connected to metamask");
+      }
     }
+  };
 
-    const fetchLandsForSale = async (account)=> {
-        // Create a Web3 instance using the provider from MetaMask
-        const web3Instance = new Web3(window.ethereum);
+  useEffect(() => {
+    initPage();
+  }, []);
 
-        // Connect to your contract using the Web3 instance
-        const contract = new web3Instance.eth.Contract(
-            contractABI,
-            landContractAddress
-        );
-
-        const gas = 2000000; 
-        // Call the getLandsForSale function on the contract
-        const result = await contract.methods.getLandsForSale().call(
-            {from: account, gas}
-        );
-        console.log(result);
-        return result;
-    }
-
-    const initPage = async () => {
-        if (window.ethereum) {
-          try {
-            const account = await getMetamaskAccount();
-            console.log(account)
-            const result = await fetchLandsForSale(account);
-            setLandsForSale(result);
-        } catch (error) {
-                console.error(error)
-                console.error('Error connecting to Metamask:', error);
-            }
-        } else {
-          if(!window.ethereum) {
-            console.error("Metamask not detected")
-          } else {
-            console.log("Already connected to metamask")
-          }
-        }
-    };
-
-    useEffect(() => {
-        initPage();
-    }, [])
-
-    return <>
-        <Sidebar />
-        <div className="card-container">
-            { landsForSale.map((land, index) => (
-            <div className="card" key={index}>
-                <h3>Land {index + 1}</h3>
-                <p>Land Id: {land.landId.toString()}</p>
-                {/* Land Owner Address */}
-                <p>Land Owner Address: {land.owner}</p>
-                <p>State: {land.identifier.state}</p>
-                <p>Division: {land.identifier.division}</p>
-                <p>District: {land.identifier.district}</p>
-                <p>Taluka: {land.identifier.taluka}</p>
-                <p>Village: {land.identifier.village}</p>
-                <p>Survery Number: {land.identifier.surveyNumber.toString()}</p>
-                <p>Subdivision: {land.identifier.subdivision}</p>
-                <p>Area: {land.area.toString()}</p>
-                <p>Purchase Date: {land.purchaseDate.toString()}</p>
-                <p>Purchase Price: {land.purchasePrice.toString()}</p>                
-                <p>Land Value at Purchase: {land.landValueAtPurchase.toString()}</p>
-                <p>Land Verified: {land.isVerified.toString()}</p>
-                <p>Land for sale: {land.isForSale.toString()}</p>
-            </div>
-            )) }
-        </div>
+  return (
+    <>
+      <Sidebar />
+      <div className="card-container">
+        {landsForSale.map((land, index) => (
+          <div className="card" key={index}>
+            <center>
+              <h3>LAND {index + 1}</h3>
+            </center>
+            <p className="land-details">
+              <b>Land Id: </b>
+              {land.landId.toString()}
+            </p>
+            {/* Land Owner Address */}
+            <p className="land-details">
+              <b>Land Owner Address: </b>
+              {land.owner}
+            </p>
+            <p className="land-details">
+              <b>State: </b>
+              {land.identifier.state}
+            </p>
+            <p className="land-details">
+              <b>Division: </b>
+              {land.identifier.division}
+            </p>
+            <p className="land-details">
+              <b>District: </b>
+              {land.identifier.district}
+            </p>
+            <p className="land-details">
+              <b>Taluka: </b>
+              {land.identifier.taluka}
+            </p>
+            <p className="land-details">
+              <b>Village: </b>
+              {land.identifier.village}
+            </p>
+            <p className="land-details">
+              <b>Survery Number: </b>
+              {land.identifier.surveyNumber.toString()}
+            </p>
+            <p className="land-details">
+              <b>Subdivision: </b>
+              {land.identifier.subdivision}
+            </p>
+            <p className="land-details">
+              <b>Area: </b>
+              {land.area.toString()}
+            </p>
+            <p className="land-details">
+              <b>Purchase Date: </b>
+              {land.purchaseDate.toString()}
+            </p>
+            <p className="land-details">
+              <b>Purchase Price: </b>
+              {land.purchasePrice.toString()}
+            </p>
+            <p className="land-details">
+              <b>Land Value at Purchase: </b>
+              {land.landValueAtPurchase.toString()}
+            </p>
+            <p className="land-details">
+              <b>Land Verified: </b>
+              {land.isVerified.toString()}
+            </p>
+            <p className="land-details">
+              <b>Land for sale: </b>
+              {land.isForSale.toString()}
+            </p>
+          </div>
+        ))}
+      </div>
     </>
+  );
 }
 
 export default LandsForSale;
