4679d57f90cfd9ab6c9895a1160bdd88e550992a
Manasvi <mamore@students.vnit.ac.in>
2024-01-25 23:16:02 +0530

    add user-verification route

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -10,9 +10,10 @@ import Received from './Components/Received/Received.jsx';
 import Sent from './Components/Sent/Sent.jsx';
 import TOO from './Components/TOO/TOO.jsx';
 import UserDetails from './Components/UserDetails/UserDetails.jsx';
+import UserVerification from './Components/UserVerification/UserVerification.jsx';
 
-const accountContractAddress = "";
-const landContractAddress = "";
+const accountContractAddress = "0xf5e301Ef2D7016604125329C2391e554b16AE6Ab";
+const landContractAddress = "0x02f45774e56EBf5C796Ed4241AC7e74450C8Ca24";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
       <Route path='/req-received' element={<Received landContractAddress={landContractAddress}/>} /> 
       <Route path='/req-sent' element={<Sent landContractAddress={landContractAddress}/>} />
       <Route path='/too' element={<TOO landContractAddress={landContractAddress}/>} />
+      <Route path='/user-verification' element={<UserVerification accountContractAddress={accountContractAddress}/>} />
     </Routes>
   );
 }
