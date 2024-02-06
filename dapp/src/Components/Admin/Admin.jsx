53a49fa96f31f98a972a3aba2de90eb849d5bf12
probablyhamzah <probablyhamzah@gmail.com>
2024-02-07 02:53:26 +0530

    added add, grant, remove, revoke for second level authority

--- dapp/src/Components/Admin/Admin.jsx
+++ dapp/src/Components/Admin/Admin.jsx
@@ -13,7 +13,7 @@ const AddLandInspector = ({accountContractAddress}) => {
   const [address, setAddress] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
 
-  const handleAddLandInspector = async () => {
+  const handleAdd = async (type) => {
     try {
       const web3 = new Web3(window.ethereum);
 
@@ -28,9 +28,15 @@ const AddLandInspector = ({accountContractAddress}) => {
       const contract = new web3.eth.Contract(contractABI, accountContractAddress);
 
       // Call the addLandInspector function on the contract
-      await contract.methods
-        .addLandInspector(address, username, phoneNumber)
-        .send({ from: deployerAddress });
+      
+      if (type == 0)
+          await contract.methods
+            .addLandInspector(address, username, phoneNumber)
+            .send({ from: deployerAddress });
+      else
+          await contract.methods
+            .addSecondLevelAuthority(address, username, phoneNumber)
+            .send({ from: deployerAddress });
 
       // Display success message or perform additional actions
       alert("Land Inspector added successfully!");
@@ -42,7 +48,7 @@ const AddLandInspector = ({accountContractAddress}) => {
   };
   
   
-  const handleGrantLandInspector = async () => {
+  const handleGrant = async (type) => {
     try {
       const web3 = new Web3(window.ethereum);
 
@@ -58,9 +64,17 @@ const AddLandInspector = ({accountContractAddress}) => {
 
       // Call the addLandInspector function on the contract
       console.log(address);
-      await contract.methods
-        .grantLandInspectorStatus(address)
-        .send({ from: deployerAddress });
+      console.log(type);
+      
+      if (type == 0)
+          await contract.methods
+            .grantLandInspectorStatus(address)
+            .send({ from: deployerAddress });
+      else
+          await contract.methods
+                .grantSecondLevelAuthorityStatus(address)
+                .send({ from: deployerAddress });
+          
 
       // Display success message or perform additional actions
       alert("Land Inspector status granted successfully!");
@@ -71,8 +85,7 @@ const AddLandInspector = ({accountContractAddress}) => {
     }
   };
   
-  
-  const handleRemoveLandInspector = async () => {
+  const handleRevoke = async (type) => {
     try {
       const web3 = new Web3(window.ethereum);
 
@@ -87,9 +100,50 @@ const AddLandInspector = ({accountContractAddress}) => {
       const contract = new web3.eth.Contract(contractABI, accountContractAddress);
 
       // Call the addLandInspector function on the contract
-      await contract.methods
-        .removeLandInspector(address)
-        .send({ from: deployerAddress });
+      console.log(address);
+      if (type == 0)
+          await contract.methods
+            .revokeLandInspectorStatus(address)
+            .send({ from: deployerAddress });
+      else
+          await contract.methods
+            .revokeSecondLevelAuthorityStatus(address)
+            .send({ from: deployerAddress });
+
+
+      // Display success message or perform additional actions
+      alert("Land Inspector status revoked successfully!");
+    } catch (error) {
+      console.error("Error revoking Land Inspector:", error);
+      // Handle errors or display an error message to the user
+      alert("Error revoking Land Inspector");
+    }
+  };
+  
+  
+  const handleRemove = async (type) => {
+    try {
+      const web3 = new Web3(window.ethereum);
+
+      // Connect to the user's MetaMask provider
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+
+      // Get the user's accounts
+      const accounts = await web3.eth.getAccounts();
+      const deployerAddress = accounts[0];
+
+      // Create a contract instance
+      const contract = new web3.eth.Contract(contractABI, accountContractAddress);
+
+      // Call the addLandInspector function on the contract
+      if (type == 0)
+          await contract.methods
+            .removeLandInspector(address)
+            .send({ from: deployerAddress });
+      else
+        await contract.methods
+            .removeSecondLevelAuthority(address)
+            .send({ from: deployerAddress });
 
       // Display success message or perform additional actions
       alert("Land Inspector removed successfully!");
@@ -135,7 +189,7 @@ const AddLandInspector = ({accountContractAddress}) => {
                 onChange={(e) => setPhoneNumber(e.target.value)}
               />
             </div>
-      <button className="submit-admin" onClick={handleAddLandInspector}>Add Land Inspector</button>
+      <button className="submit-admin" onClick={() => handleAdd(0)}>Add Land Inspector</button>
       </div>
       
       <div className="header"><div className="text">Grant Land Inspector</div><div className="underline-add"> </div></div>
@@ -151,7 +205,23 @@ const AddLandInspector = ({accountContractAddress}) => {
                 onChange={(e) => setAddress(e.target.value)}
               />
             </div>
-      <button className="submit-admin" onClick={handleGrantLandInspector}>Grant Land Inspector Status</button>
+      <button className="submit-admin" onClick={() => handleGrant(0)}>Grant Land Inspector Status</button>
+      </div>
+      
+      <div className="header"><div className="text">Revoke Land Inspector</div><div className="underline-add"> </div></div>
+      
+      <div className="inputs">
+     
+            <div className="input">
+              <img src={user_icon} alt="" />
+              <input
+                type="text"
+                placeholder="USER ADDRESS"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+      <button className="submit-admin" onClick={() => handleRevoke(0)}>Revoke Land Inspector Status</button>
       </div>
       
       
@@ -167,9 +237,88 @@ const AddLandInspector = ({accountContractAddress}) => {
                 onChange={(e) => setAddress(e.target.value)}
               />
             </div>
-      <button className="submit-admin" onClick={handleRemoveLandInspector}>Remove Land Inspector</button>
+      <button className="submit-admin" onClick={() => handleRemove(0)}>Remove Land Inspector</button>
       </div>
       
+      <div className="header"><div className="text">Add Second Level Authority</div><div className="underline-add"> </div></div>
+      <div className="inputs">
+      <div className="input">
+              <img src={user_icon} alt="" />
+              <input
+                type="text"
+                placeholder="USER NAME"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="input">
+              <img src={user_icon} alt="" />
+              <input
+                type="text"
+                placeholder="USER ADDRESS"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+      <div className="input">
+              <img src={phone_icon} alt="" />
+              <input
+                type="number"
+                placeholder="PHONE NUMBER"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+            </div>
+      <button className="submit-admin" onClick={() => handleAdd(1)}>Add Second Level Authority</button>
+      </div>
+      
+      <div className="header"><div className="text">Grant Second Level Authority</div><div className="underline-add"> </div></div>
+      
+      <div className="inputs">
+     
+            <div className="input">
+              <img src={user_icon} alt="" />
+              <input
+                type="text"
+                placeholder="USER ADDRESS"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+      <button className="submit-admin" onClick={() => handleGrant(1)}>Grant Second Level Authority Status</button>
+      </div>
+      
+      <div className="header"><div className="text">Revoke Second Level Authority</div><div className="underline-add"> </div></div>
+      
+      <div className="inputs">
+     
+            <div className="input">
+              <img src={user_icon} alt="" />
+              <input
+                type="text"
+                placeholder="USER ADDRESS"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+      <button className="submit-admin" onClick={() => handleRevoke(1)}>Revoke Second Level Authority Status</button>
+      </div>
+      
+      
+      <div className="header"><div className="text">Remove Second Level Authority</div><div className="underline-add"> </div></div>
+      <div className="inputs">
+     
+            <div className="input">
+              <img src={user_icon} alt="" />
+              <input
+                type="text"
+                placeholder="USER ADDRESS"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+      <button className="submit-admin" onClick={() => handleRemove(1)}>Remove Second Level Authority</button>
+      </div>
       
     </div>
     </div>
